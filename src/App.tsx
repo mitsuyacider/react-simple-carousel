9732f11fd65e4a3b9d7513fd6129cc13ab1c298b
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2021-02-06 10:44:43 +0100

    Add carousel content components.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ import Indicator from './components/Indicator';
 import ArrowButton from './components/ArrowButton';
 import CarouselContent from './components/CarouselContent';
 
+
 function App() {
   const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
+
+  const activeSlide = [...Array(5)].map((_: any, index: number) => (
+    //NOTE: Carousel content
+    < div className="content-wrap" key={index}>
+      <CarouselContent />
+    </div >
+  ));
 
   return (
     <>
@@ -16,17 +24,12 @@ function App() {
         {/* NOTE: Carousel body */}
         <div className="carousel">
           <div className="carousel__inner">
-
-            {/* NOTE: Carousel content */}
-            <div className="content-wrap">
-              <CarouselContent />
-            </div>
-
+            {activeSlide}
           </div>
         </div>
 
         {/* NOTE: Indicator */}
-        <Indicator numberOfDots={4} currentIndex={0} />
+        <Indicator numberOfDots={activeSlide.length} currentIndex={currentSlideIndex} />
 
         {/* NOTE: Controller */}
         <ArrowButton direction="left" onClick={() => console.log('on click left')} />
