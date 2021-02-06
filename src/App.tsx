bd10f70b83d770f9831fb455ed8b2e2b41cce1c4
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2021-02-06 09:52:52 +0100

    Add basic layout.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,39 @@ function App() {
     <>
       <div className="carousel-container">
 
+        {/* NOTE: Carousel body */}
+        <div className="carousel">
+          <div className="carousel__inner">
+
+            {/* NOTE: Carousel content */}
+            <div className="content-wrap">
+              <div className="carousel__content">
+                <div className="carousel__content__info info">
+                  <h1 className="info__title">Title</h1>
+                  <p className="info__description">This is a simple carousel. This is a simple carousel. This is a simple carousel. This is a simple carousel.</p>
+                </div>
+                <img src="https://via.placeholder.com/400/000000/ffffff" alt="slide content" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        {/* NOTE: Indicator */}
+        <ul className="indicator">
+          <li className="indicator__dot" role="dot" aria-selected="true"></li>
+          <li className="indicator__dot" role="dot" aria-selected="false"></li>
+          <li className="indicator__dot" role="dot" aria-selected="false"></li>
+          <li className="indicator__dot" role="dot" aria-selected="false"></li>
+        </ul>
+
+        {/* NOTE: Controller */}
+        <div className="arrow arrow--left">
+          <button><i className="arrow__icon arrow__icon--left"></i></button>
+        </div>
+        <div className="arrow arrow--right">
+          <button><i className="arrow__icon arrow__icon--right"></i></button>
+        </div>
       </div>
     </>
   );
